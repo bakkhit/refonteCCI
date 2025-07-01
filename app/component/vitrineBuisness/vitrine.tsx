8479d776a79645b6e01a3345d1a233461872d4e5
Clapseur <ganginou27.amorin@gmail.com>
2025-07-01 22:27:14 +0200

    fixed upstream

---
+++ app/component/vitrineBuisness/vitrine.tsx
@@ -0,0 +1,45 @@
+import Image from 'next/image';
+import EmploiTache from './emploiTache/emploiTache';
+import Rendu from './rendu/rendu';
+import Actu from './actualité/actualité';
+import EtudiantBuisness from './etudiantbuisness/etudiant';
+
+export default function VitrineBuisness() {
+  return (
+    <div className='mt-[10vw]'>
+      <div className='flex column'>
+        <Image
+          src={"/image/motifBackground.svg"}
+          width={100}
+          height={100}
+          alt="motif"
+          className="absolute -top-[13vw] right-0 w-[50vw] h-fit z-0"
+        />
+        <Image
+          src={"/image/motifBackground.svg"}
+          width={100}
+          height={100}
+          alt="motif"
+          className="absolute top-[15vw] right-0 w-[50vw] h-fit z-0"
+        />
+        <Image
+          src={"/image/motifBackground.svg"}
+          width={100}
+          height={100}
+          alt="motif"
+          className="absolute top-[38vw] right-0 w-[50vw] h-fit z-0"
+        />
+      </div>
+      <div className='z-10 relative'>
+        <EtudiantBuisness />
+        <div className='absolute top-[3vw] right-[3vw]'>
+          <EmploiTache />
+        </div>
+        <div className='flex ml-[2vw]'>
+          <Actu />
+          <Rendu />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/component/vitrineDigital/emploiTache/emploiTache.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function EmploiTache() {
+    return (
+        <div>
+            <div className="w-[27.5vw] h-[44vw] bg-[url('/image/emploiTache.svg')] bg-cover"></div>
+        </div>
+    );
+}
